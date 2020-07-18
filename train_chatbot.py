b216516cb13097686a680e93e3566f3942846aff
beginner782 <47564932+beginner782@users.noreply.github.com>
2020-07-18 13:35:08 +0530

    3

--- train_chatbot.py
+++ train_chatbot.py
@@ -15,7 +15,7 @@ accuracy=0.7
 
 import os
 if accuracy < 0.8:
-    os.system("curl --user 'admin:admin@123' http://192.168.0.111:8080/job/task3_job4/build?token=tweak")
+    os.system("curl --user 'admin:admin@123' http://192.168.0.113:8080/job/task3_job4/build?token=tweak")
 else:
-    os.system("curl --user 'admin:admin@123' http://192.168.0.111:8080/job/task3_job5/build?token=notify")
+    os.system("curl --user 'admin:admin@123' http://192.168.0.113:8080/job/task3_job5/build?token=notify")
 
