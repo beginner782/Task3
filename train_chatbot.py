35acb0210b3ca9cdf4325200f4d89b7753a81e71
beginner782 <47564932+beginner782@users.noreply.github.com>
2020-07-18 13:43:22 +0530

    2

--- train_chatbot.py
+++ train_chatbot.py
@@ -9,9 +9,4 @@ from keras.optimizers import SGD
 
 accuracy=0.7
 
-import os
-if accuracy < 0.8:
-    os.system("curl --user 'admin:admin@123' http://192.168.0.113:8080/job/task3_job4/build?token=tweak")
-else:
-    os.system("curl --user 'admin:admin@123' http://192.168.0.113:8080/job/task3_job5/build?token=notify")
 
